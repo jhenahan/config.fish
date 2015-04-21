4777b434cee645e6c1f15db2445baa25b6d3b9ae
Jack Henahan <jhenahan@me.com>
2015-04-21 12:29:28 -0400

    Boxen updates

--- config.fish
+++ config.fish
@@ -1,52 +1,63 @@
 # Oh My Fish is pretty cool
 set fish_path $HOME/.oh-my-fish
 
+# Boxen
+set -x BOXEN_HOME /opt/boxen
 
-# PATH stuff
-# Haskell
-set GHC_DOT_APP /Applications/ghc-7.8.3.app 
-set -x PATH $HOME/.cabal/bin $GHC_DOT_APP/Contents/bin $PATH
-set -x PATH $HOME/.haskellbin/.cabal-sandbox/bin $PATH
-set -x PATH $HOME/.haskellbin/**/.cabal-sandbox/bin $PATH
-set PATH $HOME/dev/haskell-tools/.cabal-sandbox/bin
+set -x BOXEN_BIN_DIR /opt/boxen/bin
+set -x BOXEN_CONFIG_DIR /opt/boxen/config
+set -x BOXEN_DATA_DIR /opt/boxen/data
+set -x BOXEN_ENV_DIR /opt/boxen/env.d
+set -x BOXEN_LOG_DIR /opt/boxen/log
+set -x BOXEN_SOCKET_DIR /opt/boxen/data/project-sockets
+set -x BOXEN_SRC_DIR /Users/jhenahan/src
 
-# System paths
-set PATH /usr/bin /bin /usr/sbin /sbin /opt/X11/bin $PATH                       
+set -x BOXEN_DOWNLOAD_URL_BASE "https://boxen-downloads.s3.amazonaws.com"
+
+## Homebrew
+set -x HOMEBREW_ROOT /opt/boxen/homebrew
+set -x HOMEBREW_CACHE /opt/boxen/cache/homebrew
+
+set -x BOXEN_HOMEBREW_BOTTLE_URL "https://boxen-downloads.s3.amazonaws.com/homebrew"
+
+set -x CFLAGS "-I$HOMEBREW_ROOT/include"
+set -x LDFLAGS "-L$HOMEBREW_ROOT/lib"
+
+## Man Paths
+set -x MANPATH $HOMEBREW_ROOT/share/man $MANPATH
+
+## Paths
+### System
+set PATH /usr/bin /bin /usr/sbin /sbin /opt/X11/bin
+
+### Homebrew
+set PATH $HOMEBREW_ROOT/bin $HOMEBREW_ROOT/sbin $BOXEN_BIN_DIR $PATH
 
 # TeX
 set PATH /usr/texbin $PATH                                                      
 
-# Composer global installs
-set PATH $HOME/.composer/vendor/bin $PATH
+### Haskell
+set GHC_DOT_APP /Applications/ghc-7.8.4.app 
+set HASKELL_BIN $HOME/.haskellbin
+set -x MANPATH $GHC_DOT_APP/Contents/share/man $MANPATH
+set PATH $HASKELL_BIN/.cabal-sandbox/bin $PATH
+set PATH $HASKELL_BIN/**/.cabal-sandbox/bin $PATH
+set PATH $HOME/.cabal/bin $GHC_DOT_APP/Contents/bin $PATH
 
-# Homebrew
-set PATH /usr/local/bin /usr/local/sbin /usr/local/opt/coreutils/libexec/gnubin $PATH
+### Local stubs
+set -x PATH bin $PATH
 
-# Local scripts
-set PATH $HOME/bin $PATH
-
-set -x PATH $PATH
-
-# Other paths
-
-## pkg-config
-## I vaguely remember having issues with cairo unless X11 went first, so there it is
-set -x PKG_CONFIG_PATH /opt/X11/lib/pkgconfig /usr/local/lib/pkgconfig
-
-## man paths, good for fish_update_completions
-set -x MANPATH /usr/local/share/man /usr/local/opt/coreutils/libexec/gnuman /usr/local/opt/erlang/lib/erlang/man $HOME/GHC/share/man $MANPATH
-
-## Conveniences
-### Hub: wrapper around git for interacting fluently with github - github/hub
+## GitHub
+set -x BOXEN_GITHUB_LOGIN jhenahan
 eval (hub alias -s)
 
 ## Language envs
 set fish_plugins chruby
-chruby ruby
 
 ## Convenience plugins
-set fish_plugins rake tmux vi-mode $fish_plugins
-set vi_mode_default vi_mode_insert
+set fish_plugins $fish_plugins
+
+source $fish_path/oh-my-fish.fish
 
 # Compilation settings
 set -x ARCHFLAGS '-arch x86_64'
@@ -74,4 +85,4 @@ alias cp 'cp -i'
 alias rm 'rm -i'
 alias mv 'mv -i'
 
-source $fish_path/oh-my-fish.fish
+chruby ruby
